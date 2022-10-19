65830a6363430b1a14026234d26c4ef3acd847e2
Olga Toporova <sova795@yandex.ru>
2022-10-19 23:25:05 +0500

    Сделаны модули с арифметическими операциями

---
+++ model_div.py
@@ -0,0 +1,9 @@
+def division(a,b):
+    return a / b
+
+def div_integer(a,b):
+    return a // b
+
+def div_remainder(a,b):
+    return a % b
+    
---
+++ model_pow.py
@@ -0,0 +1,2 @@
+def raise_to_power(a,b):
+    return a ** b

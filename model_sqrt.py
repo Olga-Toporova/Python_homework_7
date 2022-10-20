8ba78ebe3943c969254a5ab72dad875d8cdb2a63
Olga Toporova <sova795@yandex.ru>
2022-10-20 08:49:11 +0500

    внесены правки к модуль извлечения корня

--- model_sqrt.py
+++ model_sqrt.py
@@ -1,4 +1,9 @@
-from math import sqrt
+from math import sqrt as sq1
+from cmath import sqrt as sq2
 
 def square_root(a):
-    return round(sqrt(a), 2)
+    return round(sq1(a), 2)
+
+def square_root_compl(a):
+    return sq2(a)
+    
